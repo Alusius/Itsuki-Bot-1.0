955875d817127948a0a720fd47a51ccf9b16d6bb
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 13:42:57 +0700

    Update kanna.js

--- plugins/kanna.js
+++ plugins/kanna.js
@@ -1,7 +1,10 @@
-let fetch = require ('node-fetch')
-let handler = async (m, { conn }) => {
-  conn.sendButtonImg(m.chat, await (await fetch(`https://api.lolhuman.xyz/api/random/kanna?apikey=Dawnfrostkey.buffer(), 'Kanna', watermark, 'NEXT', `.kanna`, m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/kanna?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
 }
+
 handler.help = ['kanna']
 handler.tags = ['anime']
 handler.command = /^(kanna)$/i

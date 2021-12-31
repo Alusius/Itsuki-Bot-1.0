dd42402dd53d6d09860b0fc6b217da4ad0c97edd
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 20:55:40 +0700

    Update freegift.js

--- plugins/freegift.js
+++ plugins/freegift.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, args, usedPrefix }) => {
     if (args.length == 0) return conn.reply(m.chat, `Harap masukan Kode FreeGiftmu!`, m)
     if (args[0] == 'radbotz_091' || args[0] == 'followppiowy_' || args[0] == 'BloowwXx' || args[0] == 'BbL016JJQBCSr54OwwW0' || args[0] == 'giftkey01389320007' || args[0] == 'kode013923') {
 
-    if (new Date - global.db._data.users[m.sender].lastclaim > 86400000) {
+    if (new Date - global.db.data.users[m.sender].lastclaim > 86400000) {
        conn.reply(m.chat, '*🎉 SELAMAT!*\nKamu telah mendapatkan\n1000 XP ✨\n 1 Limit! 🎫\n1000 Money 💹\n1 Potion 🥤', m)
     global.db.data.users[m.sender].exp += 1000
     global.db.data.users[m.sender].limit += 1

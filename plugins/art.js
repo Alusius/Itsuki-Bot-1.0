379b04cd3ba4e931fbb4d90fb71c548843b4f941
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 14:10:09 +0700

    Create art.js

---
+++ plugins/art.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/art?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
+}
+
+handler.help = ['art']
+handler.tags = ['anime']
+handler.command = /^(art)$/i
+
+module.exports = handler

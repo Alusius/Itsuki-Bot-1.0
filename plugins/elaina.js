b020746822d7cd9c78ba91864086c1a0cf521887
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 13:42:09 +0700

    Update elaina.js

--- plugins/elaina.js
+++ plugins/elaina.js
@@ -1,7 +1,10 @@
-let fetch = require ('node-fetch')
-let handler = async (m, { conn }) => {
-  conn.sendButtonImg(m.chat, await (await fetch(`https://api.lolhuman.xyz/api/random/elaina?apikey=Dawnfrostkey.buffer(), 'Elaina', watermark, 'NEXT', `.elaina`, m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/elaina?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
 }
+
 handler.help = ['elaina']
 handler.tags = ['anime']
 handler.command = /^(elaina)$/i

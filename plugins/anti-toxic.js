6de8efd6da7be5f068dcf64d0f9c7d0d51b44996
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 13:47:12 +0700

    Update anti-toxic.js

--- plugins/anti-toxic.js
+++ plugins/anti-toxic.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./audio/uh.mp3')
+let stc = fs.readFileSync('./src/uh.mp3')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*ADA JAGOAN JADI NGERI*', 'status@broadcast')
 }
 handler.customPrefix = /anjing|memek|kontol|ngentod|goblok|goblog/i

0330f665d2f175de13bdbbd95694753dabe80b88
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 14:17:49 +0700

    Create elf.js

---
+++ plugins/elf.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/elf?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
+}
+
+handler.help = ['elf']
+handler.tags = ['anime']
+handler.command = /^(elf)$/i
+
+module.exports = handler

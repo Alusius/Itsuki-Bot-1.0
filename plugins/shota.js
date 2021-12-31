5c2428b7c4811a227d65001f949c40484157ae9f
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 14:08:22 +0700

    Update and rename handler.js.bak to plugins/shota.js

---
+++ plugins/shota.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/shota?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
+}
+
+//handler.help = ['shota']
+//handler.tags = ['anime']
+handler.command = /^(shota)$/i
+
+module.exports = handler

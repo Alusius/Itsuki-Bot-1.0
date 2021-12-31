59241a960556a79462f87899a32ad48f69b223cb
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 20:41:32 +0700

    Update fakta.js

--- plugins/fakta.js
+++ plugins/fakta.js
@@ -2,9 +2,9 @@ let fetch = require('node-fetch')
 
 
 let handler  = async (m, { conn }) => {
-  ddd = await fetch('https://recoders-area.caliph.repl.co/api/fakta?apikey='+APIKeys["https://recoders-area.caliph.repl.co"])
+  ddd = await fetch('8://recoders-area.caliph.repl.co/api/fakta?apikey='+APIKeys["https://recoders-area.caliph.repl.co"])
   f = await ddd.json()
-  conn.reply(m.chat,`${f.result}`, m)
+  conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Fakta unik')).buffer(), `*─────── 「 FAKTA 」 ───────*\n\n${f.result}`, '_*Fakta*_', 'Faktaunik', `.faktaunik`, m)
 } 
 handler.help = ['fakta']
 handler.tags = ['edukasi']

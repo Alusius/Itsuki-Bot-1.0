a3d8371c987c902cb491494db7905f9e8f1e638b
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 14:15:21 +0700

    Create shinobu.js

---
+++ plugins/shinobu.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/shinobu?apikey=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `.${command}`, m)
+
+}
+
+handler.help = ['shinobu']
+handler.tags = ['anime']
+handler.command = /^(shinobu)$/i
+
+module.exports = handler

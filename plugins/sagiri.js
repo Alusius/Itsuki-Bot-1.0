61d2cd497de5a4990f2c862dfddb8dee9e0e23a1
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 13:33:14 +0700

    Update sagiri.js

--- plugins/sagiri.js
+++ plugins/sagiri.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
-  conn.sendButtonImg(m.chat, await ( await fetch(`https://api.lolhuman.xyz/api/random/sagiri?apikey=Dawnfrostkey`))).buffer(), 'Nih Kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
+  conn.sendButtonImg(m.chat, await ( await fetch(https://api.lolhuman.xyz/api/random/sagiri?apikey=Dawnfrostkey))).buffer(), 'Nih Kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['sagiri']
 handler.tags = ['anime']

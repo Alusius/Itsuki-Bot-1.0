2d71688fdcbdbfc645f40d5079de160d81207fcc
Drz103 <88671231+Drz103@users.noreply.github.com>
2021-12-31 18:17:13 +0700

    Update imageran.js

--- plugins/imageran.js
+++ plugins/imageran.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, usedPrefix }) => {
 	
-	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.xteam.xyz/randomimage/${command}?APIKEY=Dawnfrostkey`)).buffer(), 'nih', watermark, 'NEXT', `${usedPrefix + command}`, m)
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.xteam.xyz/randomimage/${command}?APIKEY=Dawnfrostkey`)).buffer(), 'Nih kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
 
 }
 
-handler.help = ['exo <erorr>', 'bts <erorr>', 'cewe <erorr>', 'cowo <erorr>', 'blackpink <erorr>']
+handler.help = ['exo', 'bts', 'cewe', 'cowo', 'blackpink']
 handler.tags = ['image']
 handler.command = /^(exo|bts|cewe|cowo|blackpink)$/i
 

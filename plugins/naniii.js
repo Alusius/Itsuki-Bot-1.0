538be7ef01d4d04f777cb9cf798d0aec3d0fbdd4
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-01 19:08:16 +0700

    Create naniii.js

---
+++ plugins/naniii.js
@@ -0,0 +1,9 @@
+let fs = require('fs')
+let handler = async (m) => {
+let stc = fs.readFileSync('./sticker/naniii.webp')
+conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*NANI*', 'status@broadcast')
+}
+handler.customPrefix = /Naniii/i
+handler.command = new RegExp
+
+module.exports = handler

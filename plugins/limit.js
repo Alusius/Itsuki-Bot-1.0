d100fa627195414a77241ae7d8a7859343d3decc
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-01 19:18:36 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-    m.reply(`🪙*${user.limit}* Limit\n📈*${user.exp}* XP\n🧬Level *${user.level}*\n💳Role *${user.role}*`)
+    conn.sendButton(m.chat, `🪙*${user.limit}* Limit\n📈*${user.exp}* XP\n🧬Level *${user.level}*\n💳Role *${user.role}*`, watermark, 'Inventory', '.inv', m)
 }
 handler.help = ['my [@user]']
 handler.tags = ['xp']

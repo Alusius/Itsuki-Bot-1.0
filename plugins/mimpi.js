837fe83824924e3dcf301bf896f8e798dc239eb8
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-01 19:24:48 +0700

    Update mimpi.js

--- plugins/mimpi.js
+++ plugins/mimpi.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
 let stc = fs.readFileSync('./audio/mimpi.mp3')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Mimpi Lu*', 'status@broadcast')
 }
-handler.customPrefix = /mimpi|Mimpi/i
+handler.customPrefix = /mimpi|Mimpi|ngayal|hayal|halu/i
 handler.command = new RegExp
 
 module.exports = handler

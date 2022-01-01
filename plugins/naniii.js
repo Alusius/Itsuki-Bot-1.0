54baa01c789c3596d855baf11d5e69b1eae16bd4
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-02 06:54:10 +0700

    Update naniii.js

--- plugins/naniii.js
+++ plugins/naniii.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/naniii.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*NANI*', 'status@broadcast')
 }
-handler.customPrefix = /Naniii/i
+handler.customPrefix = /Nanii/i
 handler.command = new RegExp
 
 module.exports = handler

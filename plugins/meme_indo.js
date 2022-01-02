619480607c378a85ac1616c11d618a912eec751e
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-02 13:46:26 +0700

    Update meme_indo.js

--- plugins/meme_indo.js
+++ plugins/meme_indo.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
   let barbar = await url.json()
-conn.sendButtonImg(m.chat, barbar.result,'*MEME INDO*', watermark,'NEXT','.memeindo', m)
+conn.sendButtonImg(m.chat, await (await fetch(barbar.result)).buffer(), '*MEME INDO*', watermark, 'NEXT', '.memeindo', m)
 }
 handler.help = ['memeindo']
 handler.tags = ['fun']

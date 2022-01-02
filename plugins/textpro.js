70feddd9cb664b82700c530dbb1ba634a4c52c30
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-02 15:25:51 +0700

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
   let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*Daftar Efek*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  if (!effect) throw '*Daftar Efek*\n\n' + ┌─\neffects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
   let [text, ...text2] = txt.replace(effect, '').trimStart().split(split)

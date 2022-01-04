3270261eeda19efaf6c0a685d77c4f2f769681fd
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-04 19:45:07 +0700

    Update samasama.js

--- plugins/samasama.js
+++ plugins/samasama.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 let handler = async (m) => {
-let stc = fs.readFileSync('./sticker/sama2.webp')
+let stc = fs.readFileSync('./src/sama2.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*OK*', 'status@broadcast')
 }
 handler.customPrefix = /thanks/i

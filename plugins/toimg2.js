42ef35bf64f5f4a03b385afb529a5a27425cc07b
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-04 20:50:13 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -1,4 +1,19 @@
-let fs = require('fs')
+let fs = require('fs')	
+const {
+    MessageType,
+    Mimetype
+} = require("@adiwajshing/baileys");
+const anu = {
+	key : {
+           participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    documentMessage: {
+                    title: 'Hmm.jpeg', 
+                    jpegThumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+                          }
+                        }
+                      }
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
   if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
@@ -9,7 +24,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '©RadBotZ', m, false, { thumbnail: fs.readFileSync('./src/RadBot.png')})
+  await conn.sendFile(m.chat, out, 'out.png', '©RadBotZ', anu, false, { thumbnail: fs.readFileSync('./src/RadBot.png')})
 }
 handler.help = ['toimg']
 handler.tags = ['sticker']

88d00da2a43ec21b67373345aeddbf4be2bc2a7a
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-04 20:13:39 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,73 +1,30 @@
-let handler  = async (m, { itsu, usedPrefix: _p }) => {
-
-let { performance } = require('perf_hooks')
-
 let fs = require('fs')
-
-let pelo = fs.readFileSync('./src/RadBot.png')
-
-let peli = fs.readFileSync('./src/RadBotZ.jpg')
-
-function kyun(seconds){
-
-  function pad(s){
-
-    return (s < 10 ? '0' : '') + s;
-
-  }
-
-  var hours = Math.floor(seconds / (60*60));
-
-  var minutes = Math.floor(seconds % (60*60) / 60);
-
-  var seconds = Math.floor(seconds % 60);
-
-
-
-  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-
-  return `┌─〔 Status 〕\n├ Bot telah aktif selama\n├ ${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik\n└────────────`
-
-}
-
-					runtime = process.uptime()
-
-					teks = `${kyun(runtime)}`
-
-					var itsme = `0@s.whatsapp.net`
-
-					var split = `teks`
-
-					const rtimebro = {
-
-					contextInfo: {
-
-					participant: itsme,
-
-					quotedMessage: {
-
-					extendedTextMessage: {
-
-					text: split,
-
-									}
-
-								}
-
-							}
-
-					}
-
-					m.reply(teks)
-}
-
-
-
+let speed = require('performance-now')
+let handler = async (m, { conn }) => {
+            const formater1 = (seconds) => {
+                    const pad1 = (s) => {
+                        return (s < 10 ? '0' : '') + s
+                    }                   
+                    const hrs = Math.floor(seconds / (60 * 60))
+                    const mins = Math.floor(seconds % (60 * 60) / 60)
+                    const secs = Math.floor(seconds % 60)
+                    return '  ' + pad1(hrs) + ' : ' + pad1(mins) + ' : ' + pad1(secs)
+                }
+            const uptime1 = process.uptime()
+            const timestampi = speed();
+            const latensip = speed() - timestampi
+			conn.reply(m.chat, `BOT ONLINE SELAMA\n ${formater1(uptime1)}`, 'conversation', { quoted: m, contextInfo: { externalAdReply :{
+sourceUrl: 'https://wa.me/62858929626673?text=Assalamualaikum',
+mediaType: 2,
+title: `Runtime : ${formater1(uptime1)}`,
+body: 'RadBotZ By Raditya',
+thumbnailUrl: fs.readFileSync('./src/RadBotZ.jpg')
+,
+}}})
+        }
+        
 handler.help = ['runtime']
+handler.tags = ['info', 'tools']
 
-handler.tags = ['info']
-
-handler.command = /^runtime$/i
-
-
-module.exports = handler
+handler.command = /^(runtime)$/i
+module.exports = handler 

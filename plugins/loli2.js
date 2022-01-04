a97efb57503a7da1698589abfc6884b56f8da822
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-04 07:49:17 +0700

    Delete loli2.js

--- plugins/loli2.js
+++
@@ -1,8 +0,0 @@
-//Zefkiel Bot, Haruno Bot
-let handler = async(m, { conn }) => {
-conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/loli' })
-handler.help = ['loli2']
-handler.tags = ['anime', 'internet']
-handler.command = /^(loli2)$/i
-handler.limit = true
-module.export = handler

88f9578aa341b1a523a5ec835745f6888a9420b5
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-05 19:24:40 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -3,7 +3,6 @@ let handler = async (m, { conn }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 	let stc = fs.readFileSync('./src/sukses.webp')
  conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jngn comly  ya', m)
- conn.sendFile(m.chat, sukses, 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['bkp']
 handler.tags = ['bokep']

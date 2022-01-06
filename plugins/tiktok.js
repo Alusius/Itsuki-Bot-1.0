60b60c2deb55d4b2ca22eed84307842d15be6b6b
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-06 08:42:25 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -9,7 +9,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=Dawnfrostkey&url=${args[0]}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json

cd27b5f2877b2483537a30e6a33491079d309e77
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 07:57:03 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 contoh:
 *○ ${usedPrefix + command} tutup*
 *○ ${usedPrefix + command} buka*
-	`.trim(), '©RadBotZ', 'BUKA', ',grup 1', 'TUTUP', ',grup 0')
+	`.trim(), watermark, 'BUKA', ',grup 1', 'TUTUP', ',grup 0')
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)

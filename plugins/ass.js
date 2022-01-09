bc76708fb59438ffc4879d66a984c90c9e1d8581
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 07:43:14 +0700

    Update ass.js

--- plugins/ass.js
+++ plugins/ass.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
   res = await fetch(`https://api.xteam.xyz/randomimage/ass?APIKEY=HIRO`)
   heum = await res.buffer()
-  conn.sendButtonImg(m.chat, heum, 'wangy wangy wangy', '©RadBotZ', 'NEXT', `${usedPrefix + command}`, m)
+  conn.sendButtonImg(m.chat, heum, 'wangy wangy wangy', watermark, 'NEXT', `${usedPrefix + command}`, m)
 }
 handler.help = ['ass']
 handler.tags = ['hentai']

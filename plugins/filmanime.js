cd4188519e22471c964d28572e25ec62449098a4
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 07:32:14 +0700

    Update filmanime.js

--- plugins/filmanime.js
+++ plugins/filmanime.js
@@ -12,7 +12,7 @@ let handler = async(m, { conn, text }) => {
     desc,
   } = json.result
 	 	let hasil = `*🎥Film Dari ${text}*\n\n🏷️Judul : ${title}\n🔖Genre: : ${genre}\n⭐Rating : ${rating}\n🎥Link Movie : ${link}\n📖Sinopsis : ${desc}`.trim()
-    conn.sendButtonImg(m.chat, await (await fetch(json.result.thumbnail)).buffer(), hasil, '©RadBotZ', 'Internet', '.? internet', m)
+    conn.sendButtonImg(m.chat, await (await fetch(json.result.thumbnail)).buffer(), hasil, watermark, 'Internet', '.listanime', m)
 }
 handler.help = ['filmanime', 'film'].map(v => v + '<film>')
 handler.tags = ['internet', 'anime']

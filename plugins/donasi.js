c51c3491df947d691da213340689bb557b26e815
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 08:20:58 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(bank)).buffer(), `
-*─────── 「 DONATE 」 ───────*
+*────────「 DONATE 」 ────────*
 
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:

b8cdcf0441ee137d2a9a44a99409519b05f6463d
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 08:23:36 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-*─────── 「 SCRIPT 」 ───────*
+*────────「 SCRIPT 」 ────────*
 Hai 👋
-
+saya radbotz_1
 Bot Ini Menggunakan Sc dari github
 https://github.com/Drz103
 `.trim(), '©souce code', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']
-handler.command = /^(sourcecode|sc|script|github)$/i
+handler.command = /^(sourcecode|sc|scbot|script|github)$/i
 
 module.exports = handler

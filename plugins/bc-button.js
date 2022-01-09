0f3cc8e2fb351dcd0cd6435482e2b0e5f9a933fa
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 11:35:11 +0700

    Update bc-button.js

--- plugins/bc-button.js
+++ plugins/bc-button.js
@@ -6,10 +6,11 @@
 }*/
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, text }) => {
+  let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
   let thumb = 'https://telegra.ph/file/2a994c4949315bb7f51b8.jpg'
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
-  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `_* ALL BROADCAST*_`, 'Owner', '.owner', 'Menu', '.menu', /*'Donasi', '.ds'*/)
+  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n_* ALL BROADCAST*_\n${time}`, 'Owner', '.owner', 'Menu', '.menu', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
 handler.help = ['broadcast2','bc2'].map(v => v + ' <teks>')

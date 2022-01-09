5d91a2d2801d0e11382513000232f9006d6db7c0
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 07:38:57 +0700

    Update yuri.js

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -4,7 +4,7 @@ let Neko = new neko()
      let handler  = async (m, { conn, args }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
      json = (await Neko.nsfw.yuri()).url
-   conn.sendFile(m.chat, json, 'yuri.jpg', '', m, false)
+   conn.sendButtonImg(m.chat, await (await fetch(json)).buffer, 'Nih kak', watermark, 'NEXT', '.yuri', m, false)
 }
 handler.help = ['yuri']
 handler.tags = ['hentai']
@@ -14,7 +14,8 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-handler.limit = true
+
+handler.limit = true
 handler.admin = false
 handler.botAdmin = false
 

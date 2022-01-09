8a1acd061c9cb8b7fe8aefe3e95108dfce70d5a4
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-09 07:45:51 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api.lolhuman.xyz/api/random/loli?apikey=Dawnfrostkey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, 'Lomlinya kak\nterkadang gambar mengandung nsfw', 'RadBotZ×͜×', 'NEXT', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, 'Lomlinya kak\nterkadang gambar mengandung nsfw', watermark, 'NEXT', `${usedPrefix + command}`, m, false)
 }
 handler.help = ['loli']
 handler.tags = ['anime']

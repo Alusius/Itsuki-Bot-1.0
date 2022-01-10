8c7762f35a25870f5bb558fda6ec377f7dd6c5ea
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-10 20:11:22 +0700

    Update modebot.js

--- plugins/modebot.js
+++ plugins/modebot.js
@@ -1,26 +1,19 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
-let handler  = async (m, { conn, text }) => {
-	let nani = 'https://telegra.ph/file/4f4a5a3cde6a98a96da79.jpg' 
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
-  let content = conn.send2ButtonLoc(m.chat, await (await fetch(nani)).buffer(), `SILAHKAN PILIH MODE DI BAWAH INI`, watermark, 'SELF', '.off public', `PUBLIC`, `.on public`, m)
-  for (let id of chats) conn.copyNForward(id, content, true)
+let handler = async(m, { conn, command }) => {
+  let isPublic = command === "public";
+  let self = global.opts["self"]
+
+  if(self === !isPublic) return m.reply(`Dah ${!isPublic ? "Self" : "Public"} dari tadi ${m.sender.split("@")[0] === global.owner[1] ? "Mbak" : "Bang"} :v`)
+
+  global.opts["self"] = !isPublic
+
+  m.reply(`Berhasil ${!isPublic ? "Self" : "Public"} bot!`)
 }
-handler.help = ['mode']
-handler.tags = ['owner']
-handler.command = /^(mode)$/i
+
+handler.help = ["self", "public"]
+handler.tags = ["owner"]
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.command = /^(self|public)/i
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)

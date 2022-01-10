ddd20528c5a2a2810a79ab27de90d25d09f79ef3
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-10 20:46:18 +0700

    Update catalog.js

--- plugins/catalog.js
+++ plugins/catalog.js
@@ -306,7 +306,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 }
 handler.help = ['menu', '?', 'help']
 handler.tags = ['main']
-handler.command = /^(\catalog)$/i
+handler.command = /^(\!)$/i
 
 handler.register = true
 

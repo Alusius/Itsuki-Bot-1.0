b83af59ba551f449666e292a3b92d7a52bb18f91
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-10 19:43:16 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,28 +1,20 @@
-let handler  = async (m, { conn, text }) => {
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
-  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
-  let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 Broadcast 」'), true).catch(_=>_)
-  m.reply('Selesai Broadcast All Chat :)')
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+    let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
+    conn.reply(m.chat, `_Send a broadcast message to ${groups.length} group_\nestimation complete ${groups.length * 1.5} seconds`, m)
+    for (let id of groups) {
+        await delay(1500)
+        await conn.sendButtonLoc(id, await (await fetch(image)).buffer(), '*—「 Broadcast 」—*\n' + text, watermark, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
+    }
+    m.reply('_*Broadcast Finished*_')
 }
-handler.help = ['broadcast','bc'].map(v => v + ' <teks>')
+handler.help = ['bc', 'broadcast'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)$/i
+
 handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+const delay = time => new Promise(res => setTimeout(res, time))

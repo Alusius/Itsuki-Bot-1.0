0eab3dc0a185e04feebe3aed3da53c96ba280616
Drz103 <88671231+Drz103@users.noreply.github.com>
2022-01-12 07:45:08 +0700

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -1,4 +1,6 @@
+const { MessageType } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
+let fs = require('fs')
 let handler = async (m, { conn, text }) => {
   let ext= `
 *BIODATA OWNER*
@@ -17,14 +19,39 @@ let handler = async (m, { conn, text }) => {
 *Github* : https://github.com/Drz103
 
 _Oke udah itu aja terimakasih_
+`
+let name = await conn.getName(m.sender)
 
-`.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+
+  sumberImg = fs.readFileSync(`./src/RadBotZ.jpg`)
+  image = (await conn.prepareMessage('0@s.whatsapp.net', sumberImg, MessageType.image, { thumbnail: Buffer.alloc(0) })).message.imageMessage
+  res = await conn.prepareMessageFromContent(m.chat, {
+    "productMessage": {
+      "product": {
+        "productImage": image,
+        "productId": "4938174216214248",
+        "title": "───────[ OWNER ]───────",
+        "description": '\n\n' + ext,
+        "retailerId": "Itu Owner ku ya ^~^",
+        "url": '',
+        "descriptionCount": "999999999",
+        "productImageCount": "1",
+      },
+      "businessOwnerJid": "62858929626673@s.whatsapp.net",
+      "contextInfo": {
+        "forwardingScore": 9999,
+        "isForwarded": false
+      }
+    }
+  },
+    { quoted: fkon })
+  conn.relayWAMessage(res)
 
 }
-handler.help = ['owner', 'creator']
+handler.help = ['infoowner', 'infocreator']
 handler.tags = ['info']
-handler.command = /^infowner$/i
+handler.command = /^(infoowner|infocreator)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false

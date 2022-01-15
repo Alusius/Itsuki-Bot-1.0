a24be831ace3b0c5a6e9dbdb1041b38052ec2793
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-16 06:43:56 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -2,12 +2,13 @@ let fs = require('fs')
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
+    let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
     let logo = fs.readFileSync('./src/RadBotZ.jpg')
     let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
     conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
     for (let id of chats) {
         await delay(1500)
-        await conn.send2ButtonLoc(id, logo,'*——————「 Broadcast 」——————*\n' + text, watermark, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
+        await conn.send2ButtonLoc(id, logo,'*——————「 Broadcast 」——————*\n' + text, watermark + `\n\n${time}`, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
     }
     m.reply('_*Broadcast Finished*_')
 }

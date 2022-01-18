2f79332012d4ea5fd592ffd3e28ac4633aaed4d3
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-18 13:50:32 +0000

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -49,7 +49,7 @@ handler.all = async function (m, { isBlocked }) {
     let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
     let isSalam = reg.exec(m.text)
     if (isSalam && !m.fromMe) {
-        m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
+        this.sendSticker(m.chat, fs.readFileSync('./src/salam.webp'), m)
     }
 
     // backup db

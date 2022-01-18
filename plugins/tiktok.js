3683d033592d9509cb210f05b59b48d3602b5497
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-18 20:22:49 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,36 +1,65 @@
-/*
-Gausah Beli Apikey X Team lagi broo
-Mahal, mending pake lolhuman aja
-Setiap ada yang eror langsung di fix sama dia
-Tiktod By X-RyuuZeyy
-*/
-
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-m.reply(wait)
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=Dawnfrostkey&url=${args[0]}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-let { description, author, statistic, link } = json.result
-await conn.sendFile(m.chat, link, 'tt.mp4', `
-▶ ${statistic.playCount} Views
-❤ ${statistic.diggCount} Likes
-🔁 ${statistic.shareCount} Shares
-💬 ${statistic.commentCount} Comments
-- *By:* ${author.nickname} (${author.username})
+let fs = require('fs')
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+let anu = {
+	 key: { 
+          fromMe: false,
+	      participant: `0@s.whatsapp.net`, ...(m.chat ? 
+	 { remoteJid: "6289643739077-1613049930@g.us" } : {}) 
+                },
+	 message: { 
+                 "videoMessage": { 
+                 "title":"hallo bang",
+                 "h": `Hmm`,
+                 'seconds': '182', 
+                 'caption': 'Nothing to see',
+                 'jpegThumbnail': fs.readFileSync('./src/RadBotZ.jpg')
+                        }
+                       }
+	                  }
+  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} https://tiktokxxxx`
+  let { video, description, music, author } = await tiktok(args[0])
+  let url = video.no_watermark || video.with_watermark || video.no_watermark_raw || music
+  if (!url) throw 'Gagal mengambil url download'
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+- *By:* ${author.nickname} (${author.unique_id})
 - *Desc:*
 ${description}
-`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+`.trim(), anu)
 }
-
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-//handler.command = /^tiktok$/i
+
 handler.command = /^(tik(tok)?(dl)?)$/i
-handler.limit = true
+
 module.exports = handler
 
-
-//await conn.sendFile(m.chat, url, 'tiktok.mp4', kasihcaption, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+const axios = require('axios')
+async function tiktok(url) {
+  try {
+    let results = {}
+    if (/v[tm]\.tiktok\.com/g.test(url)) {
+      let res = await axios.get(url)
+      url = res.request.res.responseUrl
+    }
+    let key = await axios.get(`https://api.snaptik.site/video-key?video_url=${url}`)
+    key = JSON.parse(JSON.stringify(key.data, null, 2))
+    if (key.status !== 'success') throw key
+    let data = await axios.get(`https://api.snaptik.site/video-details-by-key?key=${key.data.key}`)
+    data = JSON.parse(JSON.stringify(data.data, null, 2))
+    if (data.status !== 'success') throw data
+    results = {
+      author: { ...data.data.author },
+      description: data.data.description,
+      video: {
+        with_watermark: `https://api.snaptik.site/download?key=${data.data.video.with_watermark}&type=video`,
+        no_watermark: `https://api.snaptik.site/download?key=${data.data.video.no_watermark}&type=video`,
+        no_watermark_raw: data.data.video.no_watermark_raw
+      },
+      music: `https://api.snaptik.site/download?key=${data.data.music}&type=music`
+    }
+    return results
+  } catch (e) {
+    throw 'Mungkin video private?!'
+  }
+}

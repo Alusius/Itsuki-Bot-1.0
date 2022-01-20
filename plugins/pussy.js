e56cfe13d331ed3f66af3f3b11c526d8cd780956
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-20 16:41:27 +0700

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
   m.reply('Sedang Diproses...')
-  bzz = await fetch(`https://api.xteam.xyz/randomimage/pussy?apikey=Dawnfrostkey`)
+  bzz = await fetch(`https://api.xteam.xyz/randomimage/pussy?apikey=apivproject`)
   res = await bzz.buffer()
   conn.sendButtonImg(m.chat, res, 'mpsss ahhh', watermark, 'NEXT', `${usedPrefix + command}`, m, false)
 }

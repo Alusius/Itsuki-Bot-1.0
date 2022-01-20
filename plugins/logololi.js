d711e091207e4df33a50b5092918de22982f52d8
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-20 13:26:22 +0700

    Update logololi.js

--- plugins/logololi.js
+++ plugins/logololi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=wVLiUzSa`
+  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=ELYASXD`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text|text>')

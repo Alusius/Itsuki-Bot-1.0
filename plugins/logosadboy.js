0e0b173d6181f3c551c401e5af0f9440d510b569
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-20 13:27:13 +0700

    Update logosadboy.js

--- plugins/logosadboy.js
+++ plugins/logosadboy.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/sadboy?text=${response[0]}&text2=${response[0]}&apikey=wVLiUzSa`
+  let res = `https://caliphapi.com/api/sadboy?text=${response[0]}&text2=${response[0]}&apikey=ELYASXD`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['sadboylogo'].map(v => v + ' <text|text>')

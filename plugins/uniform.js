37ccbf8f9e90dc6d65b4968ce660716a838257fb
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-20 16:44:04 +0700

    Update uniform.js

--- plugins/uniform.js
+++ plugins/uniform.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
   m.reply(global.wait)
-  let res = await (await fetch(`https://api.xteam.xyz/randomimage/uniform?apikey=Dawnfrostkey`)).buffer()
+  let res = await (await fetch(`https://api.xteam.xyz/randomimage/uniform?apikey=apivproject`)).buffer()
   conn.sendButtonImg(m.chat, res, `wangy wangy wangy`, watermark, 'Next',`${usedPrefix + command}`, m, false)
 }
 handler.help = ['uniform <erorr>']

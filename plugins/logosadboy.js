ba275cc5ef2a2354e84a3787416fcaa946d02a76
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-22 10:44:13 +0700

    Update logosadboy.js

--- plugins/logosadboy.js
+++ plugins/logosadboy.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('proses..')
   let res = `https://bx-hunter.herokuapp.com/api/canvas/gfx3?apikey=FuckBitch&text1=${response[0]}&text2=${response[1]}`
-  conn.sendFile(m.chat, res, 'phlogo.jpg', `Nih kak`, m, false)
+  conn.sendFile(m.chat, res, 'gfx3.jpg', `Nih kak`, m, false)
 }
-handler.help = ['sadboyloho'].map(v => v + ' <text|text>')
+handler.help = ['sadboylogo'].map(v => v + ' <text|text>')
 handler.tags = ['nulis']
 handler.command = /^(sadboylogo)$/i
 handler.register = true

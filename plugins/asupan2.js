71d480ec1a4462571655c7b7e93b80ca434f8607
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-22 07:33:14 +0000

    Update asupan2.js

--- plugins/asupan2.js
+++ plugins/asupan2.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 
 }
 
-handler.help = ['asulan']
+handler.help = ['asupan']
 handler.tags = ['internet', 'premium']
-handler.command = /^(chika|delvira|ayu|bunga|aura|nisa|ziva|yana|viona|syania|riri|syifa|mamagina|alcakenya|mangayutri|rikagusriani|asupan|bocil|gheayubie|santuy|ukhty)$/i
+handler.command = /^(chika|delvira|ayu|bunga|aura|nisa|ziva|yana|viona|syania|riri|syifa|mamagina|alcakenya|mangayutri|rikagusriani|asupan|bocil|geayubi|santuy|ukhty)$/i
 
 handler.premium = true
 

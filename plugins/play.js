e1d6864185b2a7e2c038ac3bde0caf1dab753a90
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-23 16:23:56 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim(), watermark, '🎵Audio', `.yta ${vid.url}`, '🎥Video', `.yt ${vid.url}`)
+`.trim(), watermark, '🎵EROR', `Nanii`, '🎥Video', `.yt ${vid.url}`)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']

6fa0090b68a791dc5ac3e8881c29a2dde317d1b5
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-25 07:54:07 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,12 +1,14 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://elyas-api2.herokuapp.com/api/randomimage/husbu`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, 'Nih Kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
+let handler = async (m, { conn, command, usedPrefix }) => {
+	
+	conn.sendButtonImg(m.chat, await ( await fetch(`https://api.xteam.xyz/randomimage/husbu2?APIKEY=apivproject`)).buffer(), 'Nih kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
+
 }
+
 handler.help = ['husbu']
 handler.tags = ['anime']
-handler.command = /^husbu$/i
+handler.command = /^(husbu)$/i
+handler.register = true
 
 handler.limit = true
 

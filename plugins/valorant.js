31e08800791104e8823c0e65e229191aebb60875
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-28 18:09:39 +0700

    Update valorant.js

--- plugins/valorant.js
+++ plugins/valorant.js
@@ -3,25 +3,14 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('Sedang Diproses...')
-  let res = await fetch(`http://lolhuman.herokuapp.com/api/ephoto3/valorantbanner?apikey=Dawnfrostkey&text1=${response[0]}&text2=${response[1]}&text3=${response[2]}`)
+  let res = await fetch(`http://api.lolhuman.xyz/api/ephoto3/valorantbanner?apikey=rey2k21&text1=${response[0]}&text2=${response[1]}&text3=${response[2]}`)
   conn.sendFile(m.chat, res, 'nama.jpg', `Nih Mhank`, m, false)
 }
 handler.help = ['valorant'].map(v => v + ' <teks|teks|teks>')
-handler.tags = ['sticker']
-
+handler.tags = ['nulis']
 handler.command = /^(valorant)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 handler.register = true
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
 handler.limit = true
 
 module.exports = handler

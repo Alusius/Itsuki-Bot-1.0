c780cc1652fc566b0cd82135282275a9c725b6db
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-28 18:24:38 +0700

    Update sideoppai.js

--- plugins/sideoppai.js
+++ plugins/sideoppai.js
@@ -3,23 +3,15 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args) throw 'Masukkan Parameter'
   m.reply('Sedang Diproses...')
-  let res = `https://api.lolhuman.xyz/api/random/nsfw/sideoppai?apikey=Dawnfrostkey`
+  let res = `https://api.lolhuman.xyz/api/random/nsfw/sideoppai?apikey=rey2k21`
   conn.sendFile(m.chat, res, 'side.jpg', `wangy wangy wangy`, m, false)
 }
 handler.help = ['sideoppai'].map(v => v + ' ')
 handler.tags = ['hentai']
 
 handler.command = /^(sideoppai)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
 handler.private = true
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.limit = true
 
 module.exports = handler

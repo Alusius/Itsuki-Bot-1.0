bad46254669377d33c071b26b8194253cb17bc2f
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-28 20:06:14 +0700

    Update lloli.js

--- plugins/lloli.js
+++ plugins/lloli.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
      	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-    heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=Dawnfrostkey`)
+    heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=rey2k21`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, 'Lomlinya kak\nterkadang gambar mengandung nsfw', '©RadBotZ', 'Next', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, 'Lomlinya kak\nterkadang gambar mengandung nsfw', watermark, 'NEXT', `${usedPrefix + command}`, m, false)
 }
 handler.help = ['nsfwloli']
 handler.tags = ['hentai']

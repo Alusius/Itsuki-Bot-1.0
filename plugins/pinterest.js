643cf31403d48dddf17874748acfe1dca08b209a
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-28 17:13:32 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,9 +1,10 @@
+
 let xfar = require('xfarr-api')
 let handler = async (m, { usedPrefix, command, conn, args }) => {
 	 	  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} naruto`
 xfar.Pinterest(args[0]).then(async data => {
 let pincpt = `🔗Link media : ${data.url}`
-conn.sendFile(m.chat,data.url, 'pin.jpg', pincpt,m)})
+conn.sendButtonImg(m.chat, data.url, pincpt, watermark, 'NEXT', `.pinterest args[0]`, m)})
 }
 handler.help = ['pinterest <keyword>']
 handler.tags = ['internet']

b40aa716dfeae207fa87f0c63198a8774f505492
radbotz_ <88671231+Drz103@users.noreply.github.com>
2022-01-28 20:00:17 +0700

    Update quoteislam.js

--- plugins/quoteislam.js
+++ plugins/quoteislam.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
  let handler = async (m, { conn, usedPrefix, command }) => {
- 	let heum = await fetch(`https://api.lolhuman.xyz/api/quotes/islami?apikey=Dawnfrostkey`)
+ 	let heum = await fetch(`https://api.lolhuman.xyz/api/quotes/islami?apikey=rey2k21`)
  amin = await heum.json()
     conn.sendButton(m.chat, `${amin.result}`.trim(), watermark, 'Quote Islami', `${usedPrefix + command}`)
     }

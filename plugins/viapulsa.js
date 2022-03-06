92e74b7b065931cbb0d5d0fa433a0c868d5810ac
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 00:56:47 +0800

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,33 +1,16 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Pulsa +5k
-╠➥ Cht Owner
-║
-╠═〘 Fitur 〙 ═
-║
-╠➥ *FITUR WELCOME*
-║    *FITUR STIKER*
-╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
-╠➥ *FITUR ISLAM*
-║   *QURAN*
-║   *CEK JADWAL SHALAT*
-╠➥ *INTERNET*
-║   *BRAINLY*
-║   *GOOGLE*
-╠➥ *DAN 200 LEBIH*
-║   *FITUR LAINNYA*
-║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 RAD BOT〙 ═
+╠═ Untuk Pembayaran Via Umobile
+╠➥ *60189830350*
+╠➥ Chat Owner:
+║- wa.me/60189830350
+╠═〘 LYNXZY 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
 }
-handler.help = ['viapulsa']
+handler.help = ['viaumobile']
 handler.tags = ['info']
-handler.command = /^viapulsa$/i
+handler.command = /^viaumobile$/i
 
 module.exports = handler

422c04ad8e02e4954c535e3abf416e78fc56e70b
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 01:24:58 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,19 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(bank)).buffer(), `
-*────────「 DONATE 」 ────────*
+//Jangan Di Hapus Tapi kalo Di tambah Boleh
 
-Hai 👋
-Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├ PULSA : 083813679823
-├ DANA : 085714313795
-└────
-`.trim(), 'Donasi bg Biar Bot Nya on 24Jam', 'SEWA', '.sewa')
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+╭═══ 〔 𝐃𝐎𝐍𝐀𝐒𝐈 〕 ═══
+║│➸ 𝐃𝐈𝐆𝐈:
+║│➸ *60149431385*
+║│
+║│➸ 𝐔𝐌𝐎𝐁𝐈𝐋𝐄:
+║│➸ *60189830350*
+║│
+║│➸ OWNER :
+║│➸ wa.me/60189830350
+║╰──────────────────
+╰═══════════════════
+`.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

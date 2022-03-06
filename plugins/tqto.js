500742e827a3ba6f1603b99de2e59a6ece7a96de
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 03:09:42 +0800

    Update tqto.js

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -2,31 +2,15 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(thanks)).buffer(), `
 BIG THANKS TO
 
-•Allah swt
-•My ortu
-•Nurutomo
-•Adiwajshing
-•Ariffb
-•Amel
-•IrwanX
-•Dawnfrosty
-•Rteam1
-•Beniismael
-•Raditya (gua)
-•Furqan
-•Elyas
-•Rasel
-•Xteam
-•khael
-•Atenabot
-•Baka Botz
-•ZeeoneOfc
-•Zeks
-•Rendycraft  
-•Nadin
-•Aca Mirabel
-•Penyedia Layanan API
-•Orang-orang yang Berdonasi
+➸ Allah S.W.T
+➸ My Parent
+➸ LanXzy (Owner)
+➸ LynXzy (Bot)
+➸ Exc (membantu)
+➸ Tio (membantu)
+➸ Andrii(membantu)
+➸ Penyedia Layanan API
+➸ Org2 yg berdonasi
 `.trim(), watermark, 'Back', '.menu')
 handler.help = ['Thanksto', 'tqto']
 handler.tags = ['main']

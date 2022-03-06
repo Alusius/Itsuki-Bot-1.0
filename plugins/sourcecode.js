d064ae84a27e5c406303a766325b04b706293e68
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 03:18:43 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,10 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-*────────「 SCRIPT 」 ────────*
-Hai 👋
-saya radbotz_1
-Bot Ini Menggunakan Sc dari github
-https://github.com/Drz103
+Hah sc? Desah dlu dek 🤸
 `.trim(), '©souce code', 'Thanks', '.tqto')
 handler.help = ['sourcecode']
 handler.tags = ['info']

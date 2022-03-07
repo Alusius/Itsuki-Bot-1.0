29da276a62f59de12d1564ace63e105b8f3b7814
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 19:46:21 +0800

    Update faketroli.js

--- plugins/faketroli.js
+++ plugins/faketroli.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, args, text, usedPrefix: _p }) => {
 //if(args.length == 0) return m.reply(`Cara Penggunaan: Reply Foto, Ketik *${_p}faketroli* <TextMessage> | <OrderMessage> | <OrderTitle>\nContoh: *${_p}faketroli* Hai Wibu | ZEROBOT | 10k Bang?`) 
 	let [poop, tytyd] = text.split("|")
 	if(!poop) return m.reply("Masukkan Teks Utama")
-	if(!tytyd) tytyd = "RadBotZ"
+	if(!tytyd) tytyd = "LynXzy"
 	//if(!kecil) return m.reply("Masukkan Teks Title Order!")
   try{
 //let q = m.quoted ? m.quoted : m

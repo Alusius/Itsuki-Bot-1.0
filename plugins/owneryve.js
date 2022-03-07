f80a0f83c24cc29c3d8a1dbf57fc84bb824b4c0c
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-07 20:00:46 +0800

    Update owneryve.js

--- plugins/owneryve.js
+++ plugins/owneryve.js
@@ -1,8 +1,8 @@
 let handler = function (m) {
-	this.sendContact(m.chat, '994409869765', 'Owner RadBotZ :)', m)
+	this.sendContact(m.chat, '60189830350', 'Owner Bot Lyn :)', m)
 }
 
-handler.customPrefix = ['🍭Owner Mike'] 
+handler.customPrefix = ['Owner bot'] 
 handler.command = new RegExp
 
 module.exports = handler

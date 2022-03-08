5e258f3eb5975b2f533787835e66e0cc63eb4c64
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-08 21:59:32 +0800

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -70,7 +70,7 @@ handler.help = ['jadibot']
 handler.tags = ['jadibot']
 
 handler.command = /^jadibot$/i
-handler.premium = true
+handler.premium = false
 handler.private = true
 
 handler.limit = 1

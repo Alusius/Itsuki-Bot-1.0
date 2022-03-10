30f0dd302470fa916d383bb71b4ce11875628858
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-11 01:02:21 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(bank)).buffer(), `
-*────────「 DONATE 」 ────────*
-
+*───────「 DONATE 」 ─────*
+𝙃𝙖𝙣𝙮𝙖 𝙢𝙚𝙣𝙚𝙧𝙞𝙢𝙖 𝙢𝙖𝙩𝙖 𝙪𝙖𝙣𝙜 𝙢𝙖𝙡𝙖𝙮𝙨𝙞𝙖
 ╭═══════════════
 ║╭─── [ DONASI ] ───
 ║│➸ _*UMOBILE*_✅ : 
@@ -10,11 +10,11 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ║│➸ _*DIGI*_✅: 
 ║│- 60149431385
 ║│
-║│➸ _*Owner bot*_
+║│➸ _*OWNER*_
 ║│- wa.me/60189830350
 ║╰────────────
 ╰═══════════════
-`.trim(), 'Donasi jan asal mencet', 'Owner Bot💌', '.owner')
+`.trim(), ' ', 'Owner Bot', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

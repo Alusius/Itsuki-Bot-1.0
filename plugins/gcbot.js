e819cc0b9c681205a2bc369db8f9dd3d36953b8c
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-11 01:34:45 +0800

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -1,15 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
   let ext= `
-╭═══════════════
+╭═════════════════
 ║╭──❉ [ GRUB BOT ] ❉──
-║│ 
 ║│➸ *𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋¹*
 ║│➸ https://tinyurl.com/y7xymnkd
 ║│➸ *𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋²*
 ║│➸ https://tinyurl.com/yc92fva7
 ║╰───────────────
-╰════════════════
+╰══════════════════
 
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'GRUB SIRAD')).buffer(), ext, 'LynXzy💌', 'SIAP BANG🗿', 'AUTO JOIN', 'Donasi', '.donasi', m)

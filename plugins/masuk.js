32539ffb26d77cfe4de6e227cf3458f3fda1df57
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-11 01:24:41 +0800

    Update masuk.js

--- plugins/masuk.js
+++ plugins/masuk.js
@@ -1,5 +1,5 @@
 function handler(m) {
-    m.reply('Konfirmasi ke owner untuk informasi lebih lanjut.')
+    m.reply('Chat ke owner untuk informasi lebih lanjut.')
     this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
     this.sendContact(m.chat, '60189830350@s.whatsapp.net', 'Owner Bot', m)
   }

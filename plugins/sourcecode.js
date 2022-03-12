d8c2c79e3e7148d7036dbb18b964c0003cf2de95
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-12 10:27:54 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Hah sc? Desah dlu dek 🤸
-`.trim(), '©LynXzy🥀', 'Bacod👎', 'canda')
+Hah sc? Desah dlu deck 🗿
+`.trim(), '©shirobotz', 'Iya Banh', 'canda')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i

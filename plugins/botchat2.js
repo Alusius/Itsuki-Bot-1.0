f5a2690608f933943e3caa3de33c4803ba3f412f
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-12 10:08:54 +0700

    Update botchat2.js

--- plugins/botchat2.js
+++ plugins/botchat2.js
@@ -3,7 +3,7 @@ let fs = require('fs')
 let handler = async (m, { conn, command, text }) => {
 let img = './src/RadBot.png'
 let user = global.db.data.users[m.sender]
-conn.sendButton(m.chat, `Halo juga kak ${conn.getName(m.sender)}`, 'LynXzy', 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+conn.sendButton(m.chat, `Halo juga kak ${conn.getName(m.sender)}`, 'Shiro-Botz', 'Menu', '#Menu', { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/RadBotZ.jpg')
 }}})
 }
 handler.customPrefix = /^(Hallo|halo|Halo|hallo|hi|helo|hai|hi|Hai|Helo|Hello)$/i

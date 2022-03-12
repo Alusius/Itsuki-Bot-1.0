88e57cab624d6ea3e921d654be10f95e9138e450
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-12 10:26:21 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -4,7 +4,7 @@ let info = `╠═〘 PEMBAYARAN 〙 ═
 ╠➥ *6285212023766*
 ╠➥ Chat Owner:
 ║- wa.me/6287892711054
-╠═〘 LYNXZY 〙 ═
+╠═〘 Hyzer 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 

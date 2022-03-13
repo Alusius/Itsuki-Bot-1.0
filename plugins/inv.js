7537e5a5165e065b9dab8e118670f942ca187581
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-14 05:31:08 +0700

    Update inv.js

--- plugins/inv.js
+++ plugins/inv.js
@@ -119,7 +119,7 @@ ${readMore}
 Warn: *${warn}*
 Banned: *No*
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), str, '©LynXzy💌', 'PROFILE', '.profile', 'Shop', '.shop', m)
+    await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), str, '©shirobotz', 'PROFILE', '.profile', 'Shop', '.shop', m)
 }
 handler.help = ['inventory', 'inv']
 handler.tags = ['rpg']

72a2c85215a53b997a6a043742140841f7cd3b1e
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-03-20 08:22:38 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Minggu* :      *Rm 5.00*
-╠➥ *2 Minggu* :      *Rm 10.00*
-╠➥ *3 Minggu* : *Rm 15.00*
-╠➥ *1 Bulan* :   *Rm 20.00*
+╠➥ *1 Minggu* :      *Rp 5.000*
+╠➥ *1 Bulan* :      *Rp 10.000*
+╠➥ *Permanen* : *Rp 25.000*
+╠➥ *Trial* :   *Rp 0*
 ║
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
 ╠➥ Topup/Pin
@@ -12,7 +12,7 @@ let handler = async (m, { conn }) => {
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Klil Button Di Bawah
 ║
-╠═〘 Hyzer 〙 ═`.trim(), '©shirobotz', 'Digi', '#viadigi', 'Umobile', '#viaumobile', m)
+╠═〘 Hyzer 〙 ═`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
 }
 
 handler.command = /^sewabot$/i

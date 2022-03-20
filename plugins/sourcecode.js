9f6b42d87c5db589c085e2f49542d89e61487bf6
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-03-20 09:13:47 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Hah sc? Desah dlu deck 🗿
-`.trim(), '©shirobotz', 'Iya Banh', 'canda')
+Sc Nya Liat Di https://youtu.be/0wz53xj_5hI
+Jgn Lupa Di Subscribe yaa
+`.trim(), '❖ Kanao-Bot', 'Siap Banh🗿', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
